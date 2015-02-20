b3157154aefeda7b9350e43b3713f7a25ccd6921
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2015-02-19 18:00:59 -0800

    py

--- apps/useradmin/src/useradmin/settings.py
+++ apps/useradmin/src/useradmin/settings.py
@@ -17,7 +17,7 @@
 DJANGO_APPS = [ "useradmin" ]
 NICE_NAME = "User Admin"
 REQUIRES_HADOOP = False
-ICON = "/useradmin/static/art/icon_useradmin_48.png"
+ICON = "${ STATIC_URL }/useradmin/art/icon_useradmin_48.png"
 MENU_INDEX = 60
 
 PERMISSION_ACTIONS = (
