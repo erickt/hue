249e55aca2896a3c4aad17a9468be27f028f6c48
Erick Tryzelaar <erickt@cloudera.com>
2015-07-28 15:58:20 -0700

    [indexer] indexer.install_examples doesn't return `fields` or `unique_key`

--- desktop/libs/indexer/src/indexer/tests.py
+++ desktop/libs/indexer/src/indexer/tests.py
@@ -109,5 +109,3 @@ class TestIndexerWithSolr:
     content = json.loads(resp.content)
 
     assert_equal(content.get('status'), 0)
-    assert_equal(content.get('fields'), 0)
-    assert_equal(content.get('unique_key'), 0)
